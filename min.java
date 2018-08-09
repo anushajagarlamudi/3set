02efe46b9e79934f3bace8a38942f484c39cf8ba
anushajagarlamudi <41891193+anushajagarlamudi@users.noreply.github.com>
2018-08-09 11:37:50 +0530

    Update min.java

--- min.java
+++ min.java
@@ -8,7 +8,7 @@ class Ideone
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
-				int n=sc.nextInt();
+	        int n=sc.nextInt();
 		int a[]=new int[n];
 		int min;
 		for(int i=0;i<n;i++)
@@ -18,7 +18,7 @@ class Ideone
 		min=a[0];
 		for(int i=0;i<n;i++)
 		{
-			if(min<a[i])
+			if(min>a[i])
 		    {
 		    	  min=a[i];
 		    }
